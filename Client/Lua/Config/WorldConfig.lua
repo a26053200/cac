2d1f80084245ad94b427198c0d17f9cd0c259f27
a26053200 <zhengnan262@qq.com>
2019-01-09 01:20:32 +0800

    游戏lua首次提交

---
+++ Client/Lua/Config/WorldConfig.lua
@@ -0,0 +1,39 @@
+WorldConfig = {}
+
+WorldConfig.TempA = {
+    debugName = "临时场景A",
+    level = "TempA",
+    levelUrl = "",
+}
+WorldConfig.TempB = {
+    debugName = "临时场景B",
+    level = "TempB",
+    levelUrl = "",
+}
+WorldConfig.Loading = {
+    debugName = "加载场景",
+    level = "Loading",
+    sceneName = "Loading",
+    levelUrl = "",
+}
+WorldConfig.Login = {
+    debugName = "登陆场景",
+    level = "Temp",
+    sceneName = "Login",
+    levelUrl = "",
+    needLoading = false
+}
+WorldConfig.GuideScene = {
+    debugName = "新手引导场景",
+    level = "GuideNewbie",
+    sceneName = "GuideNewbie",
+    levelUrl = "BundleScenes/GuideNewbie.unity",
+    needLoading = true
+}
+WorldConfig.Lobby = {
+    debugName = "游戏大厅",
+    level = "Lobby",
+    sceneName = "Lobby",
+    levelUrl = "BundleScenes/Lobby.unity",
+    needLoading = true
+}
